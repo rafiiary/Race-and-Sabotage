f6837e9dda492b73083cd03fb9d62460eb8fc642
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-05-24 03:17:56 -0400

    Instantiated car AI, added basic environment for testing

---
+++ Race and Sabotage/Assets/Scripts/CameraStable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStable : MonoBehaviour
+{
+    public GameObject TheCar;
+    public float carX;
+    public float carY;
+    public float carZ;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        carX = TheCar.transform.eulerAngles.x;
+        carY = TheCar.transform.eulerAngles.y;
+        carZ = TheCar.transform.eulerAngles.z;
+
+        transform.eulerAngles = new Vector3(carX - carX, carY, carZ - carZ);
+    }
+}
