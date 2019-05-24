0ae5701e12fd45c223fb1abcdc35a7e063b87ec6
Grace T <tsaosgrace@gmail.com>
2019-05-24 16:56:22 -0400

    trying to get the cars to run

---
+++ Race and Sabotage/Assets/Assets/tracker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class tracker : MonoBehaviour
+{
+
+    public GameObject TheMarker;
+    public GameObject Mark01;
+    public GameObject Mark02;
+    public GameObject Mark03;
+    public GameObject Mark04;
+    public GameObject Mark05;
+    public GameObject Mark06;
+    public int MarkTracker;
+
+    void Update()
+    {
+        if (MarkTracker == 0)
+        {
+            TheMarker.transform.position = Mark01.transform.position;
+        }
+        if (MarkTracker == 1)
+        {
+            TheMarker.transform.position = Mark02.transform.position;
+        }
+        if (MarkTracker == 2)
+        {
+            TheMarker.transform.position = Mark03.transform.position;
+        }
+        if (MarkTracker == 3)
+        {
+            TheMarker.transform.position = Mark04.transform.position;
+        }
+
+    }
+
+    IEnumerator OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Dreamcar01")
+        {
+            this.GetComponent<BoxCollider>().enabled = false;
+            MarkTracker += 1;
+            print(MarkTracker);
+            if (MarkTracker == 3)
+            {
+                MarkTracker = 0;
+            }
+            yield return new WaitForSeconds(1);
+            this.GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+}
