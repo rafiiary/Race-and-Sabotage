9557acf2a0dad13b3c337b6ada7bf12faa48fb9b
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-05-25 16:56:36 -0400

    game over winner patch

--- Race and Sabotage/Assets/Scripts/SetGameOver.cs
+++ Race and Sabotage/Assets/Scripts/SetGameOver.cs
@@ -5,12 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class SetGameOver : MonoBehaviour
 {
+    public GameObject GameOverCube;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Dreamcar01") && !FinishLine.game_over)
         {
             FinishLine.game_over = true;
-            print("added bc pushing again lol");
         }
     }
 }
