d32a3e94157dac3bf436defe0068f7e9687f9b58
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-05-25 15:27:13 -0400

    repush for debugging

--- Race and Sabotage/Assets/Scripts/SetGameOver.cs
+++ Race and Sabotage/Assets/Scripts/SetGameOver.cs
@@ -10,6 +10,7 @@ public class SetGameOver : MonoBehaviour
         if (other.gameObject.CompareTag("Dreamcar01") && !FinishLine.game_over)
         {
             FinishLine.game_over = true;
+            print("added bc pushing again lol");
         }
     }
 }
