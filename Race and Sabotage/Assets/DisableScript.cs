e882dd6a44ec04a3971ac4b26223133cb9d45371
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-05-30 12:12:06 -0400

    car AI optimized and instantiated

--- Race and Sabotage/Assets/DisableScript.cs
+++ Race and Sabotage/Assets/DisableScript.cs
@@ -8,11 +8,15 @@ public class DisableScript : MonoBehaviour
     public Button proceed;
     public Canvas CanvasToDeactivate;
     public Canvas DragAndDropCanvas;
+    public GameObject CarAI1, CarAI2, CarAI3;
     // Start is called before the first frame update
     void Start()
     {
         proceed.onClick.AddListener(Disabler);
         DragAndDropCanvas.gameObject.SetActive(false);
+        CarAI1.gameObject.SetActive(false);
+        CarAI2.gameObject.SetActive(false);
+        CarAI3.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,6 +29,9 @@ public class DisableScript : MonoBehaviour
     {
         CanvasToDeactivate.gameObject.SetActive(false);
         DragAndDropCanvas.gameObject.SetActive(true);
+        CarAI1.gameObject.SetActive(true);
+        CarAI2.gameObject.SetActive(true);
+        CarAI3.gameObject.SetActive(true);
     }
 
 
