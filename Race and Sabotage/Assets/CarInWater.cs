0b6b54741faefc1722309dbf6c860f4766755b35
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-05-30 10:43:55 -0400

    intro screen for topic added

---
+++ Race and Sabotage/Assets/CarInWater.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof(CarController))]
+    public class CarInWater : MonoBehaviour
+    {
+        private CarController car;
+        private float previousSpeed;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        void Awake()
+        {
+            car = GetComponent<CarController>();
+            previousSpeed = car.m_Topspeed;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Dreamcar01"))
+            {
+                car.m_Topspeed = 10;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag("Dreamcar01"))
+            {
+                car.m_Topspeed = previousSpeed;
+            }
+        }
+    }
+}
+
---
+++ Race and Sabotage/Assets/DisableScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisableScript : MonoBehaviour
+{
+    public Button proceed;
+    public Canvas CanvasToDeactivate;
+    public Canvas DragAndDropCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        proceed.onClick.AddListener(Disabler);
+        DragAndDropCanvas.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Disabler()
+    {
+        CanvasToDeactivate.gameObject.SetActive(false);
+        DragAndDropCanvas.gameObject.SetActive(true);
+    }
+
+
+}
