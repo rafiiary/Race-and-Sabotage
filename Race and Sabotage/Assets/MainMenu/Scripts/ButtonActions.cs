a104837eecf0ce34e7a89361e01a8d0bf9fba5a3
Arya Rafii <arya.rafii@mail.utoronto.ca>
2019-05-31 15:38:08 -0400

    Added main menu scene with some buttons

---
+++ Race and Sabotage/Assets/MainMenu/Scripts/ButtonActions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActions: MonoBehaviour
+{
+    public AudioClip buttonSound;
+    float volume = 1.0f;
+    AudioSource source;
+    void onAwake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    void PlaySound()
+    {
+        source.PlayOneShot(buttonSound, volume);
+    }
+}
