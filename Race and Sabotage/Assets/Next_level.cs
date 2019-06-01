a864e0a95fc6afb640f9ec0c9a99bf1556fab70c
Grace T <tsaosgrace@gmail.com>
2019-06-01 00:28:20 -0400

    connected the levels

---
+++ Race and Sabotage/Assets/Next_level.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Next_level : MonoBehaviour
+{
+    public GameObject guiObject;
+    public string levelToLoad;
+
+    // Use this for initialization
+    void Start()
+    {
+        guiObject.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        guiObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay(Collider other)
+    {
+        {
+            guiObject.SetActive(true);
+            if (guiObject.activeInHierarchy == true && Input.GetButtonDown("Use"))
+            {
+                Application.LoadLevel(levelToLoad);
+            }
+        }
+
+    }
+    void OnTriggerExit()
+    {
+        guiObject.SetActive(false);
+    }
+    public void NextLevel(string levelname)
+    {
+        Application.LoadLevel(levelname);
+    }
+}
