c4e925059416f1b90568bbec94064226ce14631b
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-05-31 23:43:58 -0400

    new level bases; progress underway

---
+++ Race and Sabotage/Assets/Scripts/StartGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public Button StartButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartButton.onClick.AddListener(Starter);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Starter()
+    {
+        SceneManager.LoadScene("Level1Scene");
+    }
+}
