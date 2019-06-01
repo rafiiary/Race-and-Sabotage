860ec0b364bde8e0e9b56a31a0959448ec3edc78
Arya Rafii <arya.rafii@mail.utoronto.ca>
2019-06-01 09:59:04 -0400

    Added sound effects to glossary and the music will now keep playing through different scenes

---
+++ Race and Sabotage/Assets/MainMenu/Scripts/KeepBGMusic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepBGMusic : MonoBehaviour
+{
+
+    private static KeepBGMusic instance = null;
+    public static KeepBGMusic Instance
+    {
+        get { return instance; }
+    }
+    /* Don't destroy this object between loads */
+    void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
