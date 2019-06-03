fe72047d291d496eec5e0431bf93f4314aad1f1d
Arya Rafii <arya.rafii@mail.utoronto.ca>
2019-06-03 15:51:49 -0400

    Added a playlist of songs to play randomly when game starts

---
+++ Race and Sabotage/Assets/MainMenu/Scripts/MusicController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] public class MusicController : MonoBehaviour
+{
+    int sizeOfList = 5;
+
+    int currPlayingIndex;
+    //Random number to choose which song to play
+    int randomNumber;
+    public AudioSource theSpeaker;
+    public List<AudioClip> PlayList = new List<AudioClip>();
+    /* Generate random song to play at the start of the game */
+    void Start()
+    {
+        randomNumber = Random.Range(0, sizeOfList);
+        currPlayingIndex = randomNumber;
+        theSpeaker.PlayOneShot(PlayList[randomNumber]);
+    }
+    /* Keep checking to see if current song is over. If yes, then generate NEW random
+     * song and play that instead. Else, do nothing. */
+    void Update()
+    {
+        if (!theSpeaker.isPlaying)
+        {
+            while(randomNumber == currPlayingIndex)
+            {
+                randomNumber = Random.Range(0, sizeOfList);
+            }
+            currPlayingIndex = randomNumber;
+            theSpeaker.PlayOneShot(PlayList[randomNumber]);
+        }
+    }
+}
