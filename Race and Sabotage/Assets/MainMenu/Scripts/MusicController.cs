2e91762b80746c5e1c8f90b45d30d55c77cec2f7
Arya Rafii <arya.rafii@mail.utoronto.ca>
2019-06-03 17:58:45 -0400

    Shuffle button added to settings

--- Race and Sabotage/Assets/MainMenu/Scripts/MusicController.cs
+++ Race and Sabotage/Assets/MainMenu/Scripts/MusicController.cs
@@ -8,21 +8,39 @@ using UnityEngine;
     int currPlayingIndex;
     //Random number to choose which song to play
     int randomNumber;
+    //True iff next song has been requested;
+    bool nextSongRequest;
     public AudioSource theSpeaker;
     public List<AudioClip> PlayList = new List<AudioClip>();
+
     /* Generate random song to play at the start of the game */
     void Start()
     {
+        nextSongRequest = false;
         randomNumber = Random.Range(0, sizeOfList);
         currPlayingIndex = randomNumber;
         theSpeaker.PlayOneShot(PlayList[randomNumber]);
+    }
+
+    /* Play another song */
+    public void nextSong()
+    {
+        nextSongRequest = true;
+        theSpeaker.Stop();
+        while (randomNumber == currPlayingIndex)
+        {
+            randomNumber = Random.Range(0, sizeOfList);
+        }
+        currPlayingIndex = randomNumber;
+        theSpeaker.PlayOneShot(PlayList[randomNumber]);
+        nextSongRequest = false;
     }
 
     /* Keep checking to see if current song is over. If yes, then generate NEW random
      * song and play that instead. Else, do nothing. */
     void Update()
     {
-        if (!theSpeaker.isPlaying)
+        if (!theSpeaker.isPlaying && !nextSongRequest)
         {
             while(randomNumber == currPlayingIndex)
             {
