24caaae29d8c9677cae44d06806f4c8c9384e71b
Grace T <tsaosgrace@gmail.com>
2019-06-05 20:07:20 -0400

    changed glossary button colour

--- Race and Sabotage/Assets/Standard Assets/Vehicles/Car/Scripts/CarAIControl.cs
+++ Race and Sabotage/Assets/Standard Assets/Vehicles/Car/Scripts/CarAIControl.cs
@@ -33,7 +33,7 @@ namespace UnityStandardAssets.Vehicles.Car
         [SerializeField] [Range(0, 1)] private float m_AccelWanderAmount = 0.1f;                  // how much the cars acceleration will wander
         [SerializeField] private float m_AccelWanderSpeed = 0.1f;                                 // how fast the cars acceleration wandering will fluctuate
         [SerializeField] private BrakeCondition m_BrakeCondition = BrakeCondition.TargetDistance; // what should the AI consider when accelerating/braking?
-        [SerializeField] private bool m_Driving;                                                  // whether the AI is currently actively driving or stopped.
+        [SerializeField] public bool m_Driving;                                                  // whether the AI is currently actively driving or stopped.
         [SerializeField] private Transform m_Target;                                              // 'target' the target object to aim for.
         [SerializeField] private bool m_StopWhenTargetReached;                                    // should we stop driving when we reach the target?
         [SerializeField] private float m_ReachTargetThreshold = 2;                                // proximity to target to consider we 'reached' it, and stop driving.
---
+++ Race and Sabotage/Assets/level2script.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using TMPro;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof(CarController))]
+    public class level2script : MonoBehaviour
+    {
+        private CarController m_Car; // the car controller we want to use
+        private CarAIControl ai;
+        public GameObject input_destination;
+        private TMP_Text text;
+        public GameObject first;
+
+        private void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+            ai = GetComponent<CarAIControl>();
+            
+        }
+
+
+        private void FixedUpdate()
+        {
+            // pass the input to the car!
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            //print("H" + (h * 10000).ToString());
+            float v = CrossPlatformInputManager.GetAxis("Vertical");
+            //print("V" + v.ToString());
+#if !MOBILE_INPUT
+            float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+            if (input_destination.transform.childCount > 0)
+            {
+                //m_Car.Move(h, v, v, handbrake);
+                //print(input_destination.transform.GetChild(0).GetChild(0).GetComponent<TMP_Text>().text);
+                text = input_destination.transform.GetChild(0).GetChild(0).GetComponent<TMP_Text>();
+                m_Car.m_Topspeed = float.Parse(text.text);
+                Debug.Log("THE CAR MAX SPEED CHOSEN IS " + m_Car.m_Topspeed.ToString());
+                m_Car.Move(h, v, v, handbrake);
+                ai.SetTarget(first.transform);
+            }
+            else
+            {
+                //m_Car.Move(0, 0, 1000, 1000);
+                m_Car.m_Topspeed = 0;
+                Debug.Log("does it never reach here?");
+            }
+#else
+            //m_Car.Move(h, v, v, 0f);
+#endif
+        }
+    }
+}
