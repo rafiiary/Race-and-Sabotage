9b7a251285edaff8fdf4f7d87885e133e992d942
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-06-06 02:51:57 -0400

    scene linkage for chapters

---
+++ Race and Sabotage/Assets/Scripts/LoadChapters.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadChapters : MonoBehaviour
+{
+    public Button GoToChapters;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GoToChapters.onClick.AddListener(gotochaps);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void gotochaps()
+    {
+        SceneManager.LoadScene("ChaptersScene");
+    }
+}
