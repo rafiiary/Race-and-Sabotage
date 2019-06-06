232a1fe2c120f6b323e11b0f6b32dd3ec95f053c
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-06-06 00:39:01 -0400

    enhanced code reading in change_terrain_scene

---
+++ Race and Sabotage/Assets/Scripts/TimedCanvasController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimedCanvasController : MonoBehaviour
+{
+    public Canvas TimedCanvas;
+    public Canvas NextCanvas;
+    private int time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimedCanvas.gameObject.SetActive(true);
+        NextCanvas.gameObject.SetActive(false);
+        time = 600;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time--;
+        if (time < 0)
+        {
+            TimedCanvas.gameObject.SetActive(false);
+            NextCanvas.gameObject.SetActive(true);
+            Destroy(TimedCanvas);
+        }
+    }
+}
