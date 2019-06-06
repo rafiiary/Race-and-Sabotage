f3cf5882a49147fe4fe6d072cc0e97d65f3cc05c
Grace T <tsaosgrace@gmail.com>
2019-06-05 22:04:35 -0400

    added the arrows

---
+++ Race and Sabotage/Assets/check_for_children.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class check_for_children : MonoBehaviour
+{
+    public GameObject arrows;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrows.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.childCount > 0)
+        {
+            arrows.SetActive(true);
+        }
+    }
+}
