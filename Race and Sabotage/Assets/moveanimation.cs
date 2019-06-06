70fff59c21eed5039ac9a14e41da2bd8ba2aba1a
Grace T <tsaosgrace@gmail.com>
2019-06-06 07:49:36 -0400

    improved visuals on ChaptersScene

---
+++ Race and Sabotage/Assets/moveanimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveanimation : MonoBehaviour
+{
+    private float NewX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NewX = transform.position.x + 1;
+        transform.position = new Vector3(NewX, transform.position.y, transform.position.z);
+   
+    }
+}
