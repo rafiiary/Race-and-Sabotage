464421d042eed8ea70f08b794be907470062df04
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-06-07 11:46:31 -0400

    drag and drop implemented; bug fixes incoming

---
+++ Race and Sabotage/Assets/Scripts/Draggable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    public Transform PreferredParent = null;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //Debug.Log("Begun dragging");
+        PreferredParent = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Debug.Log("PLS DRAG PLS PLS");
+        this.transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //Debug.Log("Ended dragging");
+        this.transform.SetParent(PreferredParent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
---
+++ Race and Sabotage/Assets/Scripts/DropZone.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    //public bool droppable = true;
+
+    public static bool firstbox, secondbox, thirdbox;
+
+    public void OnDrop(PointerEventData eventData){
+
+        string statement = eventData.pointerDrag.name + " was dropped on " + gameObject.name;
+        SetTrue(statement);
+        Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+        Debug.Log("first box: " + firstbox.ToString());
+        Debug.Log("second box: " + secondbox.ToString());
+        Debug.Log("third box: " + thirdbox.ToString());
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null) // && droppable
+        {
+            d.PreferredParent = this.transform;
+            //droppable = false;
+        }
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //Debug.Log("OnPointerEnter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //Debug.Log("OnPointerExit");
+    }
+
+    void SetTrue(string statement)
+    {
+        switch (statement)
+        {
+            case "Choice2 was dropped on DestinationTop":
+                secondbox = true;
+                break;
+            case "Choice1 was dropped on DestinationMiddle":
+                firstbox = true;
+                break;
+            case "Choice3 was dropped on DestinationBottom":
+                thirdbox = true;
+                break;
+        }
+    }
+}
