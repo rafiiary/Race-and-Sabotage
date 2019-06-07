aa4a8495af478d01a487e46bbc6ba7ea00ec7b1a
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-06-07 00:20:07 -0400

    implemented drag and drop scene

---
+++ Race and Sabotage/Assets/Scripts/DragDropEndRaceScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DragDropEndRaceScript : MonoBehaviour
+{
+    public GameObject car;
+    public Canvas WinCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("this is working");
+        WinCanvas.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("PLS WORK THANK");
+        if (collision.gameObject.tag == "car1")
+        {
+            car.gameObject.SetActive(false);
+            WinCanvas.gameObject.SetActive(true);
+        }
+    }
+    
+}
