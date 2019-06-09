5f455791c42bc08a6244d06faf0610b78869ee8c
Arya Rafii <arya.rafii@mail.utoronto.ca>
2019-06-09 13:02:48 -0400

    Taught acceleration and steering

---
+++ Race and Sabotage/Assets/TutorialScene/Scripts/InstructionController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InstructionController : MonoBehaviour
+{
+    public GameObject canvas;
+    public GameObject car;
+    public TextMeshProUGUI instructions;
+    public TextMeshProUGUI intro;
+    bool accelerate, brake, steer;
+    private void Start()
+    {
+        accelerate = true;
+        brake = true;
+        steer = true;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up") && accelerate)
+        {
+            accelerate = false;
+            Debug.Log("Up arrow pressed");
+            canvas.active = false;
+            intro.text = "";
+            instructions.text = "Press the Left and Right Arrow Keys to steer Left and Right.";
+        }
+        if(car.GetComponent<Rigidbody>().position.x > 700 && steer)
+        {
+            //Pause the moving car
+            canvas.active = true;
+            if (Input.GetKey("left") || Input.GetKey("right"))
+            {
+                steer = false;
+                canvas.active = false;
+            }
+        }
+    }
+}
