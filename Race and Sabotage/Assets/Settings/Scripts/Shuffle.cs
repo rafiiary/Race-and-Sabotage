0b0bfd4ff2c7232fb478c94f0b8fed3eed677d72
Arya Rafii <arya.rafii@mail.utoronto.ca>
2019-06-09 10:42:09 -0400

    Shuffle button for changing music finally works....

---
+++ Race and Sabotage/Assets/Settings/Scripts/Shuffle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shuffle : MonoBehaviour
+{
+    MusicController ControllerScript;
+    GameObject MainAudioGO;
+    /* Find the main audio game object playing the songs */
+    void Start()
+    {
+        MainAudioGO = GameObject.Find("MainSoundtrack");
+        ControllerScript = MainAudioGO.GetComponent<MusicController>();
+    }
+    public void shuffle()
+    {
+        if (MainAudioGO == null)
+        {
+            Debug.Log("MainAudio is null");
+        }
+        else
+        {
+            Debug.Log("ControllerScript is null");
+        }
+        Debug.Log("Button clicked");
+        ControllerScript.nextSong();
+    }
+
+}
