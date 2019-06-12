669d8b6163fea9f9fdf65dc98d6c934e997e6087
Grace T <tsaosgrace@gmail.com>
2019-06-12 11:56:48 -0400

    for loop script

---
+++ Race and Sabotage/Assets/for_loops.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class for_loops : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 5000000; i > 0; i--)
+        {
+            Debug.Log(i);
+            if (i == 2)
+            {
+                Time.timeScale = 0;
+            }
+        }
+
+    }
+}
