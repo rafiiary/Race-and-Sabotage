67da11e7da0bdb2a5828b0cb37b24c1979c92534
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-06-12 11:55:54 -0400

    committing for committing

---
+++ Race and Sabotage/Assets/Scripts/OrderedLoading.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrderedLoading : MonoBehaviour
+{
+    public Canvas InitialCanvas;
+    public Canvas LaterCanvas;
+    private int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitialCanvas.gameObject.SetActive(true);
+        LaterCanvas.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
