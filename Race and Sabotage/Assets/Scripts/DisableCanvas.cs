a0af5f226e9ccc9fd87795e2803d9056ce4c3916
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-06-13 00:59:20 -0400

    mini glossary linkage to d&d2

---
+++ Race and Sabotage/Assets/Scripts/DisableCanvas.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisableCanvas : MonoBehaviour
+{
+    public Button Button;
+    public Canvas Canvas;
+    public Canvas PauseCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button.onClick.AddListener(Disappear);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Disappear()
+    {
+        Canvas.gameObject.SetActive(false);
+        PauseCanvas.gameObject.SetActive(true);
+    }
+}
---
+++ Race and Sabotage/Assets/Scripts/PauseControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseControl : MonoBehaviour
+{
+    public Button pauseButton;
+    public Canvas PauseMenu;
+    bool isPaused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseButton.onClick.AddListener(PauseGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PauseGame()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            PauseMenu.gameObject.SetActive(false);
+            isPaused = false;
+        } 
+        else
+        {
+            Time.timeScale = 0;
+            PauseMenu.gameObject.SetActive(true);
+            isPaused = true;
+        }
+    }
+}
