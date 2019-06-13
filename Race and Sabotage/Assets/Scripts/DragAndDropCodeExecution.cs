61ea82368339340c6bcf0a8bdb10a94a46a8e7a1
Grace T <tsaosgrace@gmail.com>
2019-06-13 01:26:23 -0400

    not finished code execution visual for DragAdnDrop

---
+++ Race and Sabotage/Assets/Scripts/DragAndDropCodeExecution.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDropCodeExecution : MonoBehaviour
+{
+    public GameObject if_statement;
+    public GameObject if_else_statement;
+    public GameObject if_content;
+    public GameObject if_else_content;
+    public GameObject containinTrigger;
+    public triggerStaticVariable trigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trigger = containinTrigger.GetComponent<triggerStaticVariable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trigger.turning_left)
+        {
+            Debug.Log("turning_left!");
+        }
+    }
+}
---
+++ Race and Sabotage/Assets/triggerStaticVariable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerStaticVariable : MonoBehaviour
+{
+    public bool turning_left;
+    private bool inside_trigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        turning_left = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        turning_left = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        turning_left = false;
+    }
+}
