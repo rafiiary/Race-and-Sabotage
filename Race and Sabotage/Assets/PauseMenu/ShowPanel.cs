3a84e34b8372735bc0e155977d2f6b111c9c64e2
Arya Rafii <arya.rafii@mail.utoronto.ca>
2019-06-12 21:02:45 -0400

    Added initial menu to drag and drop 2 scene with pause and resume

---
+++ Race and Sabotage/Assets/PauseMenu/ShowPanel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowPanel : MonoBehaviour
+{
+    bool paused;
+    public GameObject pausePanel;
+    private void Start()
+    {
+        pausePanel.SetActive(false);
+        paused = false;
+    }
+    public void pauseGame()
+    {
+        paused = true;
+        pausePanel.SetActive(true);
+    }
+    public void unpauseGame()
+    {
+        paused = false;
+        pausePanel.SetActive(false);
+    }
+    private void Update()
+    {
+        if (paused)
+        {
+            AudioListener.pause = true;
+            Time.timeScale = 0;
+        }
+        else
+        {
+            AudioListener.pause = false;
+            Time.timeScale = 1;
+        }
+    }
+}
