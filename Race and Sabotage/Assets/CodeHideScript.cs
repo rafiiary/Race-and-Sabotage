56326209a0c5630021f4538c29392182f33f1110
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-06-14 03:02:17 -0400

    placed all left turn objects-d&d2

---
+++ Race and Sabotage/Assets/CodeHideScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CodeHideScript : MonoBehaviour
+{
+    public Canvas LeftTurn, RightTurn, GoStraight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeftTurn.gameObject.SetActive(true);
+        RightTurn.gameObject.SetActive(true);
+        GoStraight.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
