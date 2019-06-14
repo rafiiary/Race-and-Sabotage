e03c7386096ab94b1376204836ea63ea13123ea6
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-06-14 12:42:47 -0400

    change_terrain3 code exec implemented

---
+++ Race and Sabotage/Assets/Scripts/TimeDictate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeDictate : MonoBehaviour
+{
+    public GameObject panel1, panel2, panel3;
+    private int counter = 300;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel1.gameObject.SetActive(true);
+        panel2.gameObject.SetActive(true);
+        panel3.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter--;
+        if (160 < counter && counter < 220)
+        {
+            panel2.gameObject.SetActive(false);
+            panel1.gameObject.SetActive(true);
+            panel3.gameObject.SetActive(true);
+        }
+        else if (220 < counter)
+        {
+            panel1.gameObject.SetActive(false);
+            panel2.gameObject.SetActive(true);
+            panel3.gameObject.SetActive(true);
+        }
+        else
+        {
+            panel3.gameObject.SetActive(false);
+            panel2.gameObject.SetActive(true);
+            panel1.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Race and Sabotage/Assets/StartTimeDictate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartTimeDictate : MonoBehaviour
+{
+    public Button TestButton;
+    public Canvas CodeExecutionCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TestButton.onClick.AddListener(StartExec);
+        CodeExecutionCanvas.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartExec()
+    {
+        CodeExecutionCanvas.gameObject.SetActive(true);
+    }
+}
