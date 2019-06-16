717301dd0006069769e6e7f63437bee80d531295
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-06-16 01:44:19 -0400

    currency system in place, almost

--- Race and Sabotage/Assets/Scripts/MoneyTextHandler.cs
+++ Race and Sabotage/Assets/Scripts/MoneyTextHandler.cs
@@ -15,6 +15,6 @@ public class MoneyTextHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MoneyText.text = MoneyCounter.UserMoney.ToString();
+        MoneyText.text = "YOUR MONEY: " + MoneyCounter.UserMoney.ToString();
     }
 }
