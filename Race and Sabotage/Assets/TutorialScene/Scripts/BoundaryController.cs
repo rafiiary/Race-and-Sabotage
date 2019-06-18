f6d6949d0e26383a3263fd05373cbb8ea566e4d4
Arya Rafii <arya.rafii@mail.utoronto.ca>
2019-06-18 17:24:53 -0400

    Can no longer exit map in the tutorial

---
+++ Race and Sabotage/Assets/TutorialScene/Scripts/BoundaryController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float min_x, max_x, min_z, max_y, max_z;
+}
+public class BoundaryController : MonoBehaviour
+{
+    public Boundary boundary;
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().position = new Vector3
+        (
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.min_x, boundary.max_x),
+            Mathf.Clamp(GetComponent<Rigidbody>().position.y, 0.0f, boundary.max_y),
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.min_z, boundary.max_z)
+        );
+    }
+
+}
