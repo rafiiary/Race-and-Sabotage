cd4d946651d83493dfc02dd1a4f1eed0743deb5e
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-06-20 19:44:58 -0400

    currency system added

---
+++ Race and Sabotage/Assets/Level3Scene/Scripts/CurrencyPickup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrencyPickup : MonoBehaviour
+{
+    public GameObject Car;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ENTERED HERE");
+        Debug.Log("MONEY COLLECTED");
+        MoneyCounter.UserMoney += 1;
+        Debug.Log("USER MONEY NOW IS " + MoneyCounter.UserMoney.ToString());
+        Destroy(gameObject);
+        Debug.Log("GAMEOBJECT DESTROYED");
+        //if (other.gameObject.tag == Car.gameObject.tag)
+        //{
+        //    Debug.Log("MONEY COLLECTED");
+        //    MoneyCounter.UserMoney += 1;
+        //    Debug.Log("USER MONEY NOW IS " + MoneyCounter.UserMoney.ToString());
+        //    Destroy(gameObject);
+        //    Debug.Log("GAMEOBJECT DESTROYED");
+        //}
+    }
+}
