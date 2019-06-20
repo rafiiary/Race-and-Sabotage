32b8251cdbbbe4230811e912d2537df536a1899e
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-06-20 02:38:23 -0400

    creating loading screens and establishing linkage

---
+++ Race and Sabotage/Assets/LoadingScreenControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreenControl : MonoBehaviour
+{
+    public Slider slider;
+    public string NextLevel;
+    private int counter = 0;
+    private float value;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.GetComponent<Slider>().value = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter++;
+        if (counter %4 == 0)
+        {
+            slider.GetComponent<Slider>().value += (float)0.01;
+        }
+        if (slider.GetComponent<Slider>().value >= 1)
+        {
+            SceneManager.LoadScene(NextLevel);
+        }
+    }
+}
