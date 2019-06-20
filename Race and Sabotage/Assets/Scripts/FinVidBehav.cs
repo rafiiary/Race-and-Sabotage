49d63b66587cfa7f49f24141016b40f5ad566acc
Arya Rafii <arya.rafii@mail.utoronto.ca>
2019-06-19 22:23:17 -0400

    Intro scene will automatically go to level1 after finishing video

---
+++ Race and Sabotage/Assets/Scripts/FinVidBehav.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+/* Behavior after finishing video of length length */
+public class FinVidBehav : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    void Start()
+    {
+        videoPlayer.loopPointReached += loadScene;
+    }
+    void loadScene(VideoPlayer vp)
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
