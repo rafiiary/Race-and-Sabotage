becd90542b3ce991540f6913ca8c8b050112bc88
Arya Rafii <arya.rafii@mail.utoronto.ca>
2019-06-21 09:59:36 -0400

    Added acceleration to the car so the topspeed can be visibly seen. You cannot go over the speed

---
+++ Race and Sabotage/Assets/Scripts/AccelerateCar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerateCar : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().AddForce(800f, 0f, 0f);      
+    }
+}
