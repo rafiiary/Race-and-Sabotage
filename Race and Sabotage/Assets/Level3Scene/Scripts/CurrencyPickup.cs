cf9b7a6437be1d5029a82df23da10325a8fff51a
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-06-20 20:10:54 -0400

    coin audio attempted

--- Race and Sabotage/Assets/Level3Scene/Scripts/CurrencyPickup.cs
+++ Race and Sabotage/Assets/Level3Scene/Scripts/CurrencyPickup.cs
@@ -1,15 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CurrencyPickup : MonoBehaviour
 {
     public GameObject Car;
+    public AudioClip CoinPickupSound;
+    private AudioSource CoinSound;
+    MeshRenderer renderer;
+    MeshCollider collider;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        CoinSound = GetComponent<AudioSource>();
+        renderer = gameObject.GetComponent<MeshRenderer>();
+        collider = gameObject.GetComponent<MeshCollider>();
     }
 
     // Update is called once per frame
@@ -20,19 +27,19 @@ public class CurrencyPickup : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("playing sound");
+        CoinSound.PlayOneShot(CoinPickupSound, 0.7f);
         Debug.Log("ENTERED HERE");
         Debug.Log("MONEY COLLECTED");
         MoneyCounter.UserMoney += 1;
         Debug.Log("USER MONEY NOW IS " + MoneyCounter.UserMoney.ToString());
-        Destroy(gameObject);
-        Debug.Log("GAMEOBJECT DESTROYED");
-        //if (other.gameObject.tag == Car.gameObject.tag)
-        //{
-        //    Debug.Log("MONEY COLLECTED");
-        //    MoneyCounter.UserMoney += 1;
-        //    Debug.Log("USER MONEY NOW IS " + MoneyCounter.UserMoney.ToString());
-        //    Destroy(gameObject);
-        //    Debug.Log("GAMEOBJECT DESTROYED");
-        //}
+        CoinDestroy(gameObject);
+    }
+
+    private void CoinDestroy(GameObject gameobj)
+    {
+        renderer.gameObject.SetActive(false);
+        collider.gameObject.SetActive(false);
+       
     }
 }
