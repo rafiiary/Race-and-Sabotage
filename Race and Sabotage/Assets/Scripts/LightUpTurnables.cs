6498980d96fa7c3eccd41fe61ba650dc896851be
Arya Rafii <arya.rafii@mail.utoronto.ca>
2019-06-20 21:00:48 -0400

    Change_terrain scene turnables now change color when mouse hovers over

---
+++ Race and Sabotage/Assets/Scripts/LightUpTurnables.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class LightUpTurnables : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    Color clickedColor;
+    Color originalColor;
+    public GameObject panel;
+    void Start()
+    {
+        originalColor = panel.GetComponent<Image>().color;
+        clickedColor = new Color(255f, 255f, 0f);
+        clickedColor.a = 0.5f;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        panel.GetComponent<Image>().color = clickedColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        panel.GetComponent<Image>().color = originalColor;
+    }
+}
