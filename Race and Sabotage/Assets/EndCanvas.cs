0814838871aab8339b529e9c7a4b5596251d765b
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-06-22 01:24:18 -0400

    scene3 polished

---
+++ Race and Sabotage/Assets/EndCanvas.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndCanvas : MonoBehaviour
+{
+    public Canvas EndingCanvas;
+    public GameObject car;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EndingCanvas.gameObject.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EndingCanvas.gameObject.SetActive(true);
+        car.gameObject.SetActive(false);
+    }
+}
---
+++ Race and Sabotage/Assets/StarterCanvasScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarterCanvasScript : MonoBehaviour
+{
+    public GameObject Car;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Car.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Car.gameObject.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
