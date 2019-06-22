cf5d09376e06dbd6320067a88466b7482075a165
Arya Rafii <arya.rafii@mail.utoronto.ca>
2019-06-22 08:42:54 -0400

    All coins are gold and spinning now

---
+++ Race and Sabotage/Assets/Level3Scene/Scripts/SpinCoin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinCoin : MonoBehaviour
+{
+    float speed;
+    Vector3 rotateVec;
+    private void Start()
+    {
+        rotateVec = new Vector3(0f, 1f, 0f);
+        speed = 100f;
+    }
+    private void FixedUpdate()
+    {
+        transform.Rotate(rotateVec * speed * Time.deltaTime);
+    }
+}
