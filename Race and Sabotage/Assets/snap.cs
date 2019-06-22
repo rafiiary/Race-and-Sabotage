ea8b3777427eab066f223b7390278bc42c17d17a
Grace T <tsaosgrace@gmail.com>
2019-06-21 21:39:28 -0400

    figured out how to snap things into place

---
+++ Race and Sabotage/Assets/snap.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snap : MonoBehaviour
+{
+    private bool snapped;
+    private bool timeDone;
+    private bool isIn = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        IEnumerator Example()
+        {
+            timeDone = false;
+            yield return new WaitForSeconds((float)0.2);
+            timeDone = true;
+        }
+        if (isIn)
+        {
+            Debug.Log("entered is in");
+            if (!snapped)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Debug.Log("Mouse down");
+                    snapped = true;
+                }
+                else if (Input.GetMouseButtonUp(0))
+                {
+                    snapped = false;
+                }
+            }
+            else
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Debug.Log("Mouse down");
+                    snapped = true;
+                }
+                else if (Input.GetMouseButtonUp(0))
+                {
+                    snapped = false;
+                }
+            }
+        }
+    }
+}
+
