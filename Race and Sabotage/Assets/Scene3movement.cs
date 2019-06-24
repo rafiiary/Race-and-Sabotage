1079dbd99b3cd48ed7da3872ac6a0bc59c281694
Grace T <tsaosgrace@gmail.com>
2019-06-24 00:55:06 -0400

    still working on level3scene

---
+++ Race and Sabotage/Assets/Scene3movement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using TMPro;
+using System.Collections;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof(CarController))]
+    public class Scene3movement : MonoBehaviour
+    {
+        private CarController m_Car; // the car controller we want to use
+
+        private void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+            //m_WheelColliders = GetComponent<WheelCollider[]>();
+        }
+
+        private void FixedUpdate()
+        {
+            m_Car.Move(0, 4, 0, 0);
+        }
+
+    }
+}
