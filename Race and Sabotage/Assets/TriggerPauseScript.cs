d7f8734e90eeee14810c606c08d607d33a5f406c
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-06-25 01:18:10 -0400

    pause canvas for triggers

---
+++ Race and Sabotage/Assets/TriggerPauseScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPauseScript : MonoBehaviour
+{
+    private Canvas TriggeredCanvas;
+    public Canvas PauseCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TriggeredCanvas = GetComponent<Canvas>();
+        PauseCanvas.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowPause()
+    {
+        TriggeredCanvas.gameObject.SetActive(false);
+        PauseCanvas.gameObject.SetActive(true);
+    }
+
+    public void UnshowPause()
+    {
+        TriggeredCanvas.gameObject.SetActive(true);
+        PauseCanvas.gameObject.SetActive(false);
+    }
+}
