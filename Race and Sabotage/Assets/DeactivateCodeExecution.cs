6a49410cfdd18a86770661837291f194802de32d
Grace T <tsaosgrace@gmail.com>
2019-06-28 03:23:10 -0400

    the lose colliders weren't attached for most in change_terrain -> fixed

---
+++ Race and Sabotage/Assets/DeactivateCodeExecution.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateCodeExecution : MonoBehaviour
+{
+    public GameObject codeExecution;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isActiveAndEnabled)
+        {
+            codeExecution.SetActive(false);
+        }
+    }
+}
