6ce19dfefa6db2481fb3c1bb36036e0bf9f85297
Grace T <tsaosgrace@gmail.com>
2019-07-03 02:58:50 -0400

    glossay didn't update level4 button for final scene

--- Race and Sabotage/Assets/Final_scene_scroll.cs
+++ Race and Sabotage/Assets/Final_scene_scroll.cs
@@ -8,6 +8,7 @@ public class Final_scene_scroll : MonoBehaviour
 {
     public GameObject level2button;
     public GameObject level3button;
+    public GameObject level4button;
     public TextMeshProUGUI Label;
     // Start is called before the first frame update
     void Start()
@@ -23,11 +24,25 @@ public class Final_scene_scroll : MonoBehaviour
         {
             level3button.SetActive(false);
             level2button.SetActive(false);
+            level4button.SetActive(false);
+        }
+        else if (Label.text == "Variables")
+        {
+            level3button.SetActive(false);
+            level4button.SetActive(false);
+            level2button.SetActive(true);
+        }
+        else if (Label.text == "Conditions")
+        {
+            level4button.SetActive(false);
+            level3button.SetActive(true);
+            level2button.SetActive(true);
         }
         else
         {
             level3button.SetActive(true);
             level2button.SetActive(true);
+            level4button.SetActive(true);
         }
     }
 }
