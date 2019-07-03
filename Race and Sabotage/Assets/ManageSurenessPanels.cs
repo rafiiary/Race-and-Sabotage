193297bd478747b333d530aa401a979960f81676
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-07-03 02:29:05 -0400

    added sureness checkers

---
+++ Race and Sabotage/Assets/ManageSurenessPanels.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ManageSurenessPanels : MonoBehaviour
+{
+    public GameObject StarterPanel;
+    public GameObject GlossaryPanel;
+    public GameObject SettingsPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GlossaryPanel.gameObject.SetActive(false);
+        SettingsPanel.gameObject.SetActive(false);
+        StarterPanel.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowGlossaryPanel()
+    {
+        StarterPanel.gameObject.SetActive(false);
+        GlossaryPanel.gameObject.SetActive(true);
+        SettingsPanel.gameObject.SetActive(false);
+    }
+
+    public void GoToScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void HideGlossaryPanel()
+    {
+        GlossaryPanel.gameObject.SetActive(false);
+        StarterPanel.gameObject.SetActive(true);
+        SettingsPanel.gameObject.SetActive(false);
+    }
+
+    public void ShowSettingsPanel()
+    {
+        StarterPanel.gameObject.SetActive(false);
+        GlossaryPanel.gameObject.SetActive(false);
+        SettingsPanel.gameObject.SetActive(true);
+    }
+
+    public void HideSettingsPanel()
+    {
+        SettingsPanel.gameObject.SetActive(false);
+        GlossaryPanel.gameObject.SetActive(false);
+        StarterPanel.gameObject.SetActive(true);
+    }
+}
