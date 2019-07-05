713b7d983b01599a528026146f8426a69dbdefeb
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-07-04 23:06:31 -0400

    added finisher canvases

---
+++ Race and Sabotage/Assets/LoseCubeScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoseCubeScript : MonoBehaviour
+{
+    public Canvas LoseCanvas;
+    public GameObject car;
+    public Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoseCanvas.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "ColliderFront")
+        {
+            camera.transform.SetParent(LoseCanvas.transform);
+            car.gameObject.SetActive(false);
+            LoseCanvas.gameObject.SetActive(true);
+        }
+        
+    }
+}
---
+++ Race and Sabotage/Assets/WinCylinderScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCylinderScript : MonoBehaviour
+{
+    public Canvas WinCanvas;
+    public Camera camera;
+    public GameObject Car;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //WinCanvas.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "ColliderBody")
+        {
+            camera.transform.SetParent(WinCanvas.transform);
+            Car.gameObject.SetActive(false);
+            WinCanvas.gameObject.SetActive(true);
+        }
+        
+    }
+}
