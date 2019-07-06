ffe0d0ee2b484f227caaa0872e0cd85de7933509
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-07-06 18:25:02 -0400

    newscene2 is complete

--- Race and Sabotage/Assets/ColliderCubeScript.cs
+++ Race and Sabotage/Assets/ColliderCubeScript.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ColliderCubeScript : MonoBehaviour
 {
     public Camera camera;
     public GameObject car;
     public Canvas canvas;
+    public Image WrongAnswerImage;
+    private int counter = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,15 @@ public class ColliderCubeScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (counter > 0)
+        {
+            counter--;
+            WrongAnswerImage.gameObject.SetActive(true);
+        }
+        else
+        {
+            WrongAnswerImage.gameObject.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -38,6 +49,26 @@ public class ColliderCubeScript : MonoBehaviour
         boxCollider.gameObject.SetActive(false);
         camera.transform.SetParent(car.transform);
     }
+
+    public void CheckAndUnpause()
+	{
+        if (slot.first && slot.second && slot.third)
+        {
+            AudioListener.pause = false;
+            Time.timeScale = 1;
+            canvas.gameObject.SetActive(false);
+            Debug.Log("Car was unpaused");
+            BoxCollider boxCollider = gameObject.GetComponent<BoxCollider>();
+            boxCollider.isTrigger = false;
+            boxCollider.gameObject.SetActive(false);
+            camera.transform.SetParent(car.transform);
+        }
+        else
+        {
+            Debug.Log("WRONG ANSWER HUEHUEHUE");
+            counter = 60;
+        }
+	}
 
 
 }
