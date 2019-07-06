fe5ed298d3d23da31cc306d5084d40cd677fab78
Arya Rafii <arya.rafii@mail.utoronto.ca>
2019-07-05 23:00:42 -0400

    Done modifying drag and drop

---
+++ Race and Sabotage/Assets/newD&D/correctPanelOn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class correctPanelOn : MonoBehaviour
+{
+    public string tagName;
+    bool solved;
+    GameObject droppedObject;
+    string droppedObjectTag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        droppedObjectTag = "nah";
+        solved = false;
+    }
+
+    // Update is called once per frame
+    public void updateSolved()
+    {
+        droppedObject = gameObject.transform.GetChild(0).gameObject;
+        if (droppedObject != null)
+        {
+            droppedObjectTag = droppedObject.tag;
+            if (tagName.CompareTo(droppedObjectTag) == 0)
+            {
+                Debug.Log("true");
+                solved = true;
+            }
+            else
+            {
+                Debug.Log("false");
+                solved = false;
+            }
+        }
+    }
+
+    public bool getSolved()
+    {
+        return solved;
+    }
+}
