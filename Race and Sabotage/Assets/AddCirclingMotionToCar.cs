d81270104893eaab7f7495382c37fc8ae09819df
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-07-06 00:39:30 -0400

    new scene scripts under construction

---
+++ Race and Sabotage/Assets/AddCirclingMotionToCar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+using UnityStandardAssets.Utility;
+
+public class AddCirclingMotionToCar: MonoBehaviour
+{
+    public GameObject car;
+    private WaypointProgressTracker progresstracker;
+    private CarAIControl carControl;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        progresstracker = car.GetComponent<WaypointProgressTracker>();
+        carControl = car.GetComponent<CarAIControl>();
+        //progresstracker.gameObject.SetActive(false);
+        //carControl.gameObject.SetActive(false);
+        car.AddComponent<CirclingMotion>();
+        EndCanvasTimer.TimerInt = 100;
+    }
+}
