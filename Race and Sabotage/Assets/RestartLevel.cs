6d65e9e1fed35f15b2c405724768fc979fafef13
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-07-07 01:34:34 -0400

    game level order established

---
+++ Race and Sabotage/Assets/RestartLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour
+{
+    private Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(restarter);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void restarter()
+    {
+        SceneManager.LoadScene(Application.loadedLevel);
+    }
+
+}
