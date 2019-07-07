5e6ef3e5c27f1800d703150f5d3c568bd72e9196
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-07-07 11:43:01 -0400

    glossary updated

--- Race and Sabotage/Assets/Scripts/Final_scene_scroll.cs
+++ Race and Sabotage/Assets/Scripts/Final_scene_scroll.cs
@@ -9,6 +9,7 @@ public class Final_scene_scroll : MonoBehaviour
     public GameObject level2button;
     public GameObject level3button;
     public GameObject level4button;
+    public GameObject level5button;
     public TextMeshProUGUI Label;
     // Start is called before the first frame update
     void Start()
@@ -25,24 +26,28 @@ public class Final_scene_scroll : MonoBehaviour
             level3button.SetActive(false);
             level2button.SetActive(false);
             level4button.SetActive(false);
+            level5button.SetActive(false);
         }
         else if (Label.text == "Variables")
         {
-            level3button.SetActive(false);
-            level4button.SetActive(false);
             level2button.SetActive(true);
+            level3button.SetActive(true);
+            level4button.SetActive(true);
+            level5button.SetActive(false);
         }
         else if (Label.text == "Conditions")
         {
-            level4button.SetActive(false);
+            level4button.SetActive(true);
             level3button.SetActive(true);
             level2button.SetActive(true);
+            level5button.SetActive(true);
         }
         else
         {
             level3button.SetActive(true);
             level2button.SetActive(true);
             level4button.SetActive(true);
+            level5button.SetActive(false);
         }
     }
 }
