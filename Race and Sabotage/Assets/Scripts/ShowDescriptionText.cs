f38d0bcd183ca2d83b3ef011fad9e3e3c0905cac
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-07-08 11:17:52 -0400

    description texts added for level3scene2 and newscene

---
+++ Race and Sabotage/Assets/Scripts/ShowDescriptionText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ShowDescriptionText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Text DescriptionText;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        DescriptionText.gameObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        DescriptionText.gameObject.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DescriptionText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
