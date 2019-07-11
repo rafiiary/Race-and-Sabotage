1235b7e262f81961a5fc367930404a185e797490
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-07-11 01:42:33 -0400

    animation for l3s2

---
+++ Race and Sabotage/Assets/L3S2Anim.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class L3S2Anim : MonoBehaviour
+{
+    public GameObject car;
+    public GameObject camera;
+    public void stuff()
+    {
+        car.gameObject.SetActive(true);
+        car.AddComponent<CarStraightControl>();
+        camera.gameObject.SetActive(true);
+        Destroy(gameObject);
+    }
+}
