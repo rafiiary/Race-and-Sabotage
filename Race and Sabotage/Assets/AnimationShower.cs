0143fdfe9537d245e59f8669d1aa1fa91316ef39
Grace T <tsaosgrace@gmail.com>
2019-07-11 00:32:37 -0400

    destroy gameobject

--- Race and Sabotage/Assets/AnimationShower.cs
+++ Race and Sabotage/Assets/AnimationShower.cs
@@ -8,16 +8,10 @@ using UnityStandardAssets.Vehicles.Car;
 public class AnimationShower : MonoBehaviour
 {
     public Canvas puzzlecanvas;
-    public Camera flybyCamera;
-    public Camera originalCamera;
-    private bool entered = false;
     public void CameraSwitching()
     {
-        if (!entered)
-        {
-            Debug.Log("Camera switchin beginning boyzzzzz YEET");
-            puzzlecanvas.gameObject.SetActive(true);
-            entered = true;
-        }
+        Debug.Log("Camera switchin beginning boyzzzzz YEET");
+        puzzlecanvas.gameObject.SetActive(true);
+        Destroy(gameObject);
     }
 }
