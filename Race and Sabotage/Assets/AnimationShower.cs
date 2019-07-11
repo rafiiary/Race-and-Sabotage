ecb3db979af700dfc542470a4631c77123ae04b3
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-07-10 23:53:54 -0400

    ANIMATIONS ADDED BOYSSSS WOOWOWOWWOWOWOWOWO NEWSCENE

--- Race and Sabotage/Assets/AnimationShower.cs
+++ Race and Sabotage/Assets/AnimationShower.cs
@@ -7,28 +7,10 @@ using UnityStandardAssets.Vehicles.Car;
 
 public class AnimationShower : MonoBehaviour
 {
-    public Animation animation;
-    public Camera flybycam;
     public Canvas puzzlecanvas;
-    public GameObject car;
-    private Animator animator;
-    // Start is called before the first frame update
-    void Start()
+    public void CameraSwitching()
     {
-        animator = flybycam.GetComponent<Animator>();
-        car.gameObject.SetActive(false);
-        puzzlecanvas.gameObject.SetActive(false);
-        animation.Play();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("FlybyCam"))
-        {
-            Debug.Log("ANIMATION ENDED");
-            puzzlecanvas.gameObject.SetActive(true);
-            flybycam.gameObject.SetActive(false);
-        }
+        Debug.Log("Camera switchin beginning boyzzzzz YEET");
+        puzzlecanvas.gameObject.SetActive(true);
     }
 }
