0ec9743cb91ea1adfb71d54fc1fac85d8f27fcc6
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-07-11 11:27:05 -0400

    car going underground level1scene bug fix

---
+++ Race and Sabotage/Assets/change_terrain_2_anim.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class change_terrain_2_anim : MonoBehaviour
+{
+	public GameObject timedcanvas;
+    public GameObject turntablecanvas;
+    public void stuff()
+	{
+        timedcanvas.gameObject.SetActive(true);
+        turntablecanvas.gameObject.SetActive(true);
+	}
+}
