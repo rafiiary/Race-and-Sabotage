c4c39abeaa262aa1124495c5de68ee31f0d0bd1f
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-07-11 00:54:42 -0400

    level2scene anim

---
+++ Race and Sabotage/Assets/L2SAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L2SAnimation : MonoBehaviour
+{
+    public GameObject initialcanvas;
+    public GameObject car;
+
+    public void dostuff()
+    {
+        initialcanvas.gameObject.SetActive(true);
+        car.gameObject.SetActive(true);
+        Destroy(gameObject);
+    }
+}
