5055c4a4c4f85d91c1d9c668db8752063f1fd5ed
Grace T <tsaosgrace@gmail.com>
2019-07-11 10:13:37 -0400

    newd&D2 camera fix

---
+++ Race and Sabotage/Assets/CodeExecutionTurnOff.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeExecutionTurnOff : MonoBehaviour
+{
+    public Camera flybyCamera;
+    public GameObject code;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (flybyCamera.isActiveAndEnabled)
+        {
+            code.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
--- Race and Sabotage/Assets/Scripts/OnlyActiveWhenCarIs.cs
+++ Race and Sabotage/Assets/Scripts/OnlyActiveWhenCarIs.cs
@@ -9,6 +9,8 @@ public class OnlyActiveWhenCarIs : MonoBehaviour
     public GameObject one;
     public GameObject two;
     public GameObject three;
+    public GameObject aniCamera;
+    public GameObject dragandDrop;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,13 +24,17 @@ public class OnlyActiveWhenCarIs : MonoBehaviour
     void Update()
     {
         print("car is active " + car.active.ToString());
-        if (car.active == true)
+        if (car.active == true & aniCamera.active == false & dragandDrop.active == false)
         {
             Debug.Log("car is active");
             codeExecution.SetActive(true);
             one.SetActive(true);
             two.SetActive(true);
             three.SetActive(true);
+        }
+        else
+        {
+            codeExecution.SetActive(false);
         }
     }
 }
