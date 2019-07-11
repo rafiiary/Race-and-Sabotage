e8114d0d55ee34f5c1a1fc3059960176a58d81da
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-07-11 01:19:34 -0400

    while loop anim

---
+++ Race and Sabotage/Assets/WhileLoopAnim.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoopAnim : MonoBehaviour
+{
+    public GameObject car;
+    public GameObject explaining;
+    public void stuff()
+    {
+        car.gameObject.SetActive(true);
+        explaining.gameObject.SetActive(true);
+        Destroy(gameObject);
+    }
+}
