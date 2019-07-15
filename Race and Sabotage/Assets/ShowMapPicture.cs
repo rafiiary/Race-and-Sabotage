ad4a0adc0c9dc7ea622067ce0e386a6d7adb2501
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-07-15 00:23:39 -0400

    mountain range made more visible

---
+++ Race and Sabotage/Assets/ShowMapPicture.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowMapPicture : MonoBehaviour
+{
+    public Image MapImage;
+    private int counter = 200;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MapImage.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (counter > 0)
+        {
+            MapImage.gameObject.SetActive(true);
+            counter--;
+        }
+        else
+        {
+            MapImage.gameObject.SetActive(false);
+        }
+    }
+}
