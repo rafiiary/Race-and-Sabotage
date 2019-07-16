6bbdfa438cb6f74e39fcf22a206b3efd455105f0
Grace T <tsaosgrace@gmail.com>
2019-07-16 08:47:10 -0400

    for loop 2 half done

---
+++ Race and Sabotage/Assets/ForLoop2.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof(CarController))]
+    public class ForLoop2 : MonoBehaviour
+    {
+        private CarController m_Car; // the car controller we want to use
+        public GameObject LeftBracketDrop;
+        public GameObject RightBracketDrop;
+        public GameObject VariableDrop;
+        public GameObject LoopDrop;
+        public GameObject dragAndDropCanvas;
+        public static bool correct_input = false;
+        bool right = false;
+        //public GameObject countdown;
+        public TextMeshProUGUI move_forward;
+        public TextMeshProUGUI lst;
+        public TextMeshProUGUI forLoop;
+        public TextMeshProUGUI forLoopContent;
+        public TextMeshProUGUI ActualCountDown;
+        public GameObject CanvasActualCountdownIsOn;
+        public GameObject watchCodeExecution;
+        public TextMeshProUGUI feedback;
+        private bool entered = false;
+        private bool timeDone = false;
+        private bool timeDone2 = false;
+        private bool timeDone3 = false;
+        private bool timeDone4 = true;
+        private bool timeDone5 = false;
+        public float accel;
+        private Rigidbody m_Rigidbody;
+        private WheelCollider[] m_WheelColliders = new WheelCollider[4];
+        public static float TURNING = 0;
+        private bool noCodingVersion = true;
+        public GameObject preventsRollingDown;
+        private float TURN = 0;
+        private bool if_statement_done;
+        public GameObject winCanvas;
+        public GameObject loseCanvas;
+        private bool No_moneyYet = false;
+        public GameObject Pause;
+        private bool change_if_content = true;
+        private bool drive = false;
+        private int timer = 10;
+        private int secondTimer = 5;
+        public GameObject errorMessage;
+        public TextMeshProUGUI errorMessageText;
+        private bool attemptedOnce = false;
+
+        private void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+            watchCodeExecution.SetActive(false);
+            lst.color = new Color32(150, 20, 45, 45);
+            move_forward.color = new Color32(150, 20, 45, 45);
+            forLoop.color = new Color32(150, 20, 45, 45);
+            forLoopContent.color = new Color32(150, 20, 45, 45);
+        }
+
+        private void FixedUpdate()
+        {
+            // pass the input to the car!
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            //print("H" + (h * 10000).ToString());
+            float v = CrossPlatformInputManager.GetAxis("Vertical");
+            //print("V" + v.ToString());
+#if !MOBILE_INPUT
+            float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+            //m_Car.Move(5, 2, v, handbrake)
+            if (LeftBracketDrop.transform.childCount > 0 & RightBracketDrop.transform.childCount > 0 & VariableDrop.transform.childCount > 0 & LoopDrop.transform.childCount > 0)
+            {
+                Debug.Log(LoopDrop.transform.GetChild(0).tag);
+                if (!attemptedOnce)
+                {
+                    attemptedOnce = true;
+                    Debug.Log("all the drops have more than one input now");
+                    if (LeftBracketDrop.transform.GetChild(0).tag == "[" & RightBracketDrop.transform.GetChild(0).tag == "]")
+                    {
+                    }
+                    else if (LeftBracketDrop.transform.GetChild(0).tag == "{" || LeftBracketDrop.transform.GetChild(0).tag == "}" || RightBracketDrop.transform.GetChild(0).tag == "}" || RightBracketDrop.transform.GetChild(0).tag == "{")
+                    {
+                        errorMessage.SetActive(true);
+                        errorMessageText.text = "'{}' are used for the start and end of a loop whereas lists use '[]'";
+                        StartCoroutine(error());
+                    }
+                    else
+                    {
+                        errorMessage.SetActive(true);
+                        errorMessageText.text = "The TimerCounter needs to be in a list.";
+                        StartCoroutine(error());
+                    }
+                    if (VariableDrop.transform.GetChild(0).tag != "GameController")
+                    {
+                        errorMessage.SetActive(true);
+                        errorMessageText.text = "You need a VARIABLE to go through the list eg. number";
+                        StartCoroutine(error());
+                    }
+                    if (LoopDrop.transform.GetChild(0).tag == "+2" || LoopDrop.transform.GetChild(0).tag == "-2" || LoopDrop.transform.GetChild(0).tag == "-1")
+                    {
+                        errorMessage.SetActive(false);
+                        errorMessageText.text = "no";
+                    }
+                    else
+                    {
+                        errorMessage.SetActive(true);
+                        errorMessageText.text = "Invalid input inside the loop. Try again.";
+                        StartCoroutine(error());
+                    }
+                    //{
+                    //    errorMessage.SetActive(true);
+                    //    errorMessageText.text = "YAY.";
+                    //    StartCoroutine(error());
+                    //}
+                    //else
+                    //{
+                    //    errorMessage.SetActive(true);
+                    //    errorMessageText.text = "Invalid input inside the loop. Try again.";
+                    //    StartCoroutine(error());
+                    //}
+
+                    //Pause.SetActive(true);
+                    if (!entered)
+                    {
+                        //StartCoroutine(CountdownTimer());
+                    }
+                    Debug.Log(timeDone.ToString() + "timeDone");
+                    Debug.Log(timeDone2.ToString() + "timeDone2");
+                    //dragAndDropCanvas.SetActive(false);
+                    watchCodeExecution.SetActive(true);
+                    if (drive)
+                    {
+                        if (timer != 0)
+                        {
+                            m_Car.Move(0, 2, 0, 0);
+                        }
+                        timer--;
+                        lst.color = new Color32(150, 20, 45, 45);
+                        forLoop.color = new Color32(150, 20, 45, 45);
+                        forLoopContent.color = new Color32(150, 20, 45, 45);
+
+                    }
+                }
+            }
+            else
+            {
+                attemptedOnce = false;
+            }
+
+#else
+            m_Car.Move(h, v, v, 0f);
+
+#endif
+        }
+        IEnumerator error()
+        {
+            yield return new WaitForSeconds((float)2);
+            errorMessage.SetActive(false);
+        }
+        //IEnumerator CountdownTimer()
+        //{
+        //    entered = true;
+        //    //colouring the list
+        //    lst.color = new Color32(255, 128, 0, 255);
+        //    yield return new WaitForSeconds((float)0.2);
+        //    //starting the loop colouring
+        //    forLoop.color = new Color32(255, 128, 0, 255);
+        //    yield return new WaitForSeconds((float)1);
+        //    forLoop.color = new Color32(150, 20, 45, 45);
+        //    forLoopContent.color = new Color32(255, 128, 0, 255);
+        //    yield return new WaitForSeconds((float)1);
+        //    //second number
+        //    forLoop.color = new Color32(255, 128, 0, 255);
+        //    forLoopContent.color = new Color32(150, 20, 45, 45);
+        //    yield return new WaitForSeconds((float)1);
+        //    forLoop.color = new Color32(150, 20, 45, 45);
+        //    forLoopContent.color = new Color32(255, 128, 0, 255);
+        //    yield return new WaitForSeconds((float)1);
+        //    //third number
+        //    forLoop.color = new Color32(255, 128, 0, 255);
+        //    forLoopContent.color = new Color32(150, 20, 45, 45);
+        //    yield return new WaitForSeconds((float)1);
+        //    forLoop.color = new Color32(150, 20, 45, 45);
+        //    forLoopContent.color = new Color32(255, 128, 0, 255);
+        //    yield return new WaitForSeconds((float)1);
+        //    drive = true;
+        //    CanvasActualCountdownIsOn.SetActive(false);
+        //    move_forward.color = new Color32(255, 128, 0, 255);
+        //    yield return new WaitForSeconds((float)3);
+        //    move_forward.color = new Color32(150, 20, 45, 45);
+        //    winCanvas.SetActive(true);
+        //}
+    }
+}
---
+++ Race and Sabotage/Assets/GleyPlugins/RateGame/Scripts/Editor/SettingsWindow.cs
@@ -0,0 +1,265 @@
+﻿namespace GleyRateGame
+{
+    using UnityEditor;
+    using UnityEditor.SceneManagement;
+    using UnityEngine;
+
+    public class SettingsWindow : EditorWindow
+    {
+        private RateGameSettings adSettings;
+        private DisplayConditions firstShowSettings;
+        private DisplayConditions postponeSettings;
+        private Vector2 scrollPosition = Vector2.zero;
+        private RatePopupTypes ratePopupType;
+        private string iosAppID;
+        private string googlePlayBundleID;
+        private string mainText;
+        private string yesButtonText;
+        private string noButton;
+        private string laterButton;
+        private string sendButton;
+        private string notNowButton;
+        private string neverButton;       
+        private int minStarsToSend;
+        private bool clearSave;
+        
+
+        [MenuItem("Window/Gley/Rate Game")]
+        private static void Init()
+        {
+            SettingsWindow window = (SettingsWindow)GetWindow(typeof(SettingsWindow), true, "Rate Game Settings Window - v.1.0.1");
+            window.minSize = new Vector2(520, 520);
+            window.Show();
+        }
+
+
+        private void OnEnable()
+        {
+            adSettings = Resources.Load<RateGameSettings>("RateGameSettingsData");
+            if (adSettings == null)
+            {
+                CreateAdSettings();
+                adSettings = Resources.Load<RateGameSettings>("RateGameSettingsData");
+            }
+
+            //load asset values
+            iosAppID = adSettings.iosAppID;
+            ratePopupType = adSettings.ratePopupType;
+            googlePlayBundleID = adSettings.googlePlayBundleID;
+            mainText = adSettings.mainText;
+            yesButtonText = adSettings.yesButton;
+            noButton = adSettings.noButton;
+            laterButton = adSettings.laterButton;
+            sendButton = adSettings.sendButton;
+            notNowButton = adSettings.notNowButton;
+            neverButton = adSettings.neverButton;
+            firstShowSettings = adSettings.firstShowSettings;
+            postponeSettings = adSettings.postponeSettings;
+            minStarsToSend = adSettings.minStarsToSend;
+#if UNITY_EDITOR
+            clearSave = adSettings.clearSave;
+#endif
+        }
+
+
+        private void OnGUI()
+        {
+            EditorStyles.label.wordWrap = true;
+            scrollPosition = GUILayout.BeginScrollView(scrollPosition, false, false, GUILayout.Width(position.width), GUILayout.Height(position.height));
+
+            EditorGUILayout.LabelField("Your App IDs:", EditorStyles.boldLabel);
+            iosAppID = EditorGUILayout.TextField("iOS App ID", iosAppID);
+            googlePlayBundleID = EditorGUILayout.TextField("Google Play bundle ID", googlePlayBundleID);
+            EditorGUILayout.Space();
+
+            GUILayout.Label("Customize popup text:", EditorStyles.boldLabel);
+            ratePopupType = (RatePopupTypes)EditorGUILayout.EnumPopup("Select Rate Popup type: ", ratePopupType);
+            EditorGUILayout.LabelField("Start Popup: - a popup with 5 stars selectable by user");
+            EditorGUILayout.LabelField("Yes/No Popup: - a popup that asks the user if he/she wants to rate the app");
+            EditorGUILayout.Space();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Main Popup Text:", GUILayout.Width(146));
+            var areaStyle = new GUIStyle(GUI.skin.textArea);
+            areaStyle.wordWrap = true;
+            var width = position.width - 35;
+            areaStyle.fixedHeight = 0;
+            areaStyle.fixedHeight = areaStyle.CalcHeight(new GUIContent(mainText), width);
+            mainText = EditorGUILayout.TextArea(mainText, areaStyle, GUILayout.Height(areaStyle.fixedHeight));
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space();
+
+            if (ratePopupType == RatePopupTypes.StarsPopup)
+            {
+                sendButton = EditorGUILayout.TextField("Send Button", sendButton);
+                minStarsToSend = EditorGUILayout.IntField("Min Stars to Open Store:", minStarsToSend);
+                EditorGUILayout.LabelField("Opens the store page to rate on if user gives more that " + minStarsToSend + " stars");
+                EditorGUILayout.Space();
+
+                notNowButton = EditorGUILayout.TextField("Not now button", notNowButton);
+                EditorGUILayout.LabelField("Closes the popup, but it will open again based on your conditions");
+                EditorGUILayout.Space();
+
+                neverButton = EditorGUILayout.TextField("Never button", neverButton);
+                EditorGUILayout.LabelField("Closes the popup, popup never opens again");
+                EditorGUILayout.Space();
+            }
+
+            if (ratePopupType == RatePopupTypes.YesNoPopup)
+            {
+                yesButtonText = EditorGUILayout.TextField("Yes button ", yesButtonText);
+                EditorGUILayout.LabelField("Opens the store page to rate");
+                EditorGUILayout.Space();
+
+                noButton = EditorGUILayout.TextField("No button", noButton);
+                EditorGUILayout.LabelField("Closes the popup, popup never opens again(this user does not like your game)");
+                EditorGUILayout.Space();
+
+                laterButton = EditorGUILayout.TextField("Later button (opens again later)", laterButton);
+                EditorGUILayout.LabelField("Closes the popup, but it will open again based on your conditions");
+                EditorGUILayout.Space();
+            }
+
+            EditorGUILayout.LabelField("If a button text is empty, that button will not show. At least one button should be active");
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+
+            EditorGUILayout.LabelField("Show Options:", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("First Show:", EditorStyles.boldLabel);
+            ShowDisplaySettings(firstShowSettings);
+
+            EditorGUILayout.LabelField("Postponed:", EditorStyles.boldLabel);
+            ShowDisplaySettings(postponeSettings);
+
+#if UNITY_EDITOR
+            clearSave = EditorGUILayout.Toggle("Clear Save", clearSave);
+#endif
+
+            //save settings
+            EditorGUILayout.Space();
+            if (GUILayout.Button("Save"))
+            {
+                SaveSettings();
+            }
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Open Test Scene"))
+            {
+                EditorSceneManager.OpenScene("Assets/GleyPlugins/RateGame/Example/TestScene.unity");
+            }
+
+            GUILayout.EndScrollView();
+        }
+
+
+        private void ShowDisplaySettings(DisplayConditions showSettings)
+        {
+            showSettings.useSessionsCount = EditorGUILayout.Toggle("Use Sessions Count:", showSettings.useSessionsCount);
+            if (showSettings.useSessionsCount)
+            {
+                showSettings.minSessiosnCount = EditorGUILayout.IntField("Number of Sessions:", showSettings.minSessiosnCount);
+            }
+
+            showSettings.useCustomEvents = EditorGUILayout.Toggle("Use Custom Events:", showSettings.useCustomEvents);
+            if (showSettings.useCustomEvents)
+            {
+                showSettings.minCustomEvents = EditorGUILayout.IntField("Number of Custom Events:", showSettings.minCustomEvents);
+            }
+            showSettings.useInGameTime = EditorGUILayout.Toggle("Use In Game Time:", showSettings.useInGameTime);
+            if (showSettings.useInGameTime)
+            {
+                showSettings.gamePlayTime = EditorGUILayout.IntField("Number of minutes:", showSettings.gamePlayTime);
+            }
+            showSettings.useRealTime = EditorGUILayout.Toggle("Use Real Time: ", showSettings.useRealTime);
+            if(showSettings.useRealTime)
+            {
+                showSettings.realTime = EditorGUILayout.FloatField("Number Of Hours:", showSettings.realTime);
+            }
+            if (showSettings.useSessionsCount == false && showSettings.useCustomEvents == false && showSettings.useInGameTime == false)
+            {
+                EditorGUILayout.LabelField("The rate popup will be shown when ShowRatePopup() method is called (no delay)");
+            }
+            else
+            {
+                string text = "The rate popup will be shown after";
+                if (showSettings.useSessionsCount)
+                {
+                    text += " " + showSettings.minSessiosnCount + " sessions";
+                }
+
+                if (showSettings.useCustomEvents)
+                {
+                    if (showSettings.useSessionsCount)
+                    {
+                        text += " and";
+                    }
+                    text += " " + showSettings.minCustomEvents + " custom events";
+                }
+
+                if (showSettings.useInGameTime)
+                {
+                    if (showSettings.useCustomEvents || showSettings.useSessionsCount)
+                    {
+                        text += " and";
+                    }
+                    text += " " + showSettings.gamePlayTime + " game play minutes";
+                }
+
+                if(showSettings.useRealTime)
+                {
+                    if(showSettings.useInGameTime || showSettings.useCustomEvents || showSettings.useSessionsCount)
+                    {
+                        text += " and";
+                    }
+                    text += " " + showSettings.realTime + " real time hours after app was first open";
+                }
+                EditorGUILayout.LabelField(text);
+            }
+        }
+
+
+        private void SaveSettings()
+        {
+            adSettings.iosAppID = iosAppID;
+            adSettings.googlePlayBundleID = googlePlayBundleID;
+            adSettings.ratePopupType = ratePopupType;
+            adSettings.mainText = mainText;
+            adSettings.yesButton = yesButtonText;
+            adSettings.noButton = noButton;
+            adSettings.laterButton = laterButton;
+            adSettings.sendButton = sendButton;
+            adSettings.notNowButton = notNowButton;
+            adSettings.neverButton = neverButton;
+            adSettings.firstShowSettings = firstShowSettings;
+            adSettings.postponeSettings = postponeSettings;
+            adSettings.minStarsToSend = minStarsToSend;
+            adSettings.clearSave = clearSave;
+            SetSelectedPopup();
+            EditorUtility.SetDirty(adSettings);
+        }
+
+
+        private void CreateAdSettings()
+        {
+            RateGameSettings asset = ScriptableObject.CreateInstance<RateGameSettings>();
+            if (!AssetDatabase.IsValidFolder("Assets/GleyPlugins/RateGame/Resources"))
+            {
+                AssetDatabase.CreateFolder("Assets/GleyPlugins/RateGame", "Resources");
+                AssetDatabase.Refresh();
+            }
+
+            AssetDatabase.CreateAsset(asset, "Assets/GleyPlugins/RateGame/Resources/RateGameSettingsData.asset");
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+
+
+        private void SetSelectedPopup()
+        {
+            GameObject popup = AssetDatabase.LoadAssetAtPath("Assets/GleyPlugins/RateGame/Prefabs/" + ratePopupType.ToString() + ".prefab", typeof(GameObject)) as GameObject;
+            adSettings.popupGameObject = popup;
+            GameObject canvas = AssetDatabase.LoadAssetAtPath("Assets/GleyPlugins/RateGame/Prefabs/RatePopupCanvas.prefab", typeof(GameObject)) as GameObject;
+            adSettings.ratePopupCanvas = canvas;
+        }
+    }
+}
