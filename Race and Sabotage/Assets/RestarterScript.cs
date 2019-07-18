55ee6516d27490c973f8e1d6fe58593373a3ff28
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-07-18 00:11:23 -0400

    loading screen sample finalized

---
+++ Race and Sabotage/Assets/RestarterScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RestarterScript : MonoBehaviour
+{
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("REACHED HERE BOYS LMAOOOOOO");
+        slider.value = 0.9f;
+    }
+}
