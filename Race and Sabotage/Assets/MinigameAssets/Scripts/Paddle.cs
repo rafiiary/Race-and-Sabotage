f36d16cdcfc27cbd3746d523b4ed1cdd8b4c8def
Grace T <tsaosgrace@gmail.com>
2019-07-20 00:08:11 -0400

    adjusting the script or mouse

--- Race and Sabotage/Assets/MinigameAssets/Scripts/Paddle.cs
+++ Race and Sabotage/Assets/MinigameAssets/Scripts/Paddle.cs
@@ -5,15 +5,26 @@ public class Paddle : MonoBehaviour {
 
 	public bool autoPlay = false;
 	public float minX, maxX;
+    private bool mouseIn = false;
 
 	private Ball ball;
 	
 	void Start () {
 		ball = GameObject.FindObjectOfType<Ball>();
-	}
+    }
 		
 	// Update is called once per frame
 	void Update () {
+        Rect screenRect = new Rect(0, 0, Screen.width, Screen.height);
+        if (screenRect.Contains(Input.mousePosition))
+        {
+            mouseIn = true;
+
+        }
+        else
+        {
+            mouseIn = false;
+        }
 		if (!autoPlay) {
 			MoveWithMouse();
 		} else {
@@ -29,9 +40,27 @@ public class Paddle : MonoBehaviour {
 	}
 	
 	void MoveWithMouse () {
-		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
-		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
-		paddlePos.x = Mathf.Clamp(mousePosInBlocks, minX, maxX);
-		this.transform.position = paddlePos;
-	}
+        //Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
+        Rect screenRect = new Rect(0, 0, Screen.width, Screen.height);
+        if (!screenRect.Contains(Input.mousePosition))
+            return;
+        else
+        {
+            this.transform.position = new Vector3(Camera.main.ScreenToViewportPoint(Input.mousePosition).x, this.transform.position.y, this.transform.position.z);
+        }
+        float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        
+        Debug.Log(Input.mousePosition.x);
+        //Debug.DrawRay(Camera.main.ScreenPointToRay(Input.mousePosition), forward, Color.green);
+        //paddlePos.x = Mathf.Clamp(mousePosInBlocks, minX, maxX);
+        //RaycastHit hit;
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //if (Physics.Raycast(ray, out hit, 100.0f))
+        //{
+        //    Debug.Log("it has entered");
+        //    this.transform.position = new Vector3(this.transform.position.x + Input.mousePosition.x, this.transform.position.y);
+        //}
+        //this.transform.position = new Vector3(Input.mousePosition.x, this.transform.position.y, 0f); ;
+    }
 }
