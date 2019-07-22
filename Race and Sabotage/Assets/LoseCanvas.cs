2e02dcfec6278d754e2d0e260f1e62ed77c1a55e
Grace T <tsaosgrace@gmail.com>
2019-07-21 20:23:19 -0400

    fixed camera issue in level2scene:

---
+++ Race and Sabotage/Assets/LoseCanvas.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCanvas : MonoBehaviour
+{
+    public GameObject car;
+    public GameObject codeExecution;
+    public GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isActiveAndEnabled)
+        {
+            camera.transform.SetParent(this.transform);
+            car.SetActive(false);
+            codeExecution.SetActive(false);
+        }
+    }
+}
