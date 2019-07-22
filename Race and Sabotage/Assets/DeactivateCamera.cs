bf2ad60b8cc1b6663c2596fe662d2c6f5b9a4d4a
Grace T <tsaosgrace@gmail.com>
2019-07-22 03:12:37 -0400

    reattaches the caerma in ifvswhile now

---
+++ Race and Sabotage/Assets/DeactivateCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateCamera : MonoBehaviour
+{
+    public GameObject camera;
+    public GameObject car;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isActiveAndEnabled)
+        {
+            camera.transform.SetParent(this.transform, false);
+            car.SetActive(false);
+        }
+    }
+}
