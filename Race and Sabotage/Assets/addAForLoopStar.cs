4debe4c383a5866b11c7ea5fbfc24a36262a418f
Arya Rafii <arya.rafii@mail.utoronto.ca>
2019-07-23 13:27:28 -0400

    Achievement page will now show player achievements

---
+++ Race and Sabotage/Assets/addAForLoopStar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addAForLoopStar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DetermineStars.doneForLoop();   
+    }
+}
