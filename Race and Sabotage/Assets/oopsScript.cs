aa4263beec79f3fba6190f8768be8817470bab61
rafiiary <arya.rafii@mail.utoronto.ca>
2019-07-31 02:42:53 -0400

    Made the oops I messed up combo. Press LShift and Escape simultaneously

---
+++ Race and Sabotage/Assets/oopsScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class oopsScript : MonoBehaviour
+{
+    private static oopsScript instance = null;
+    public static oopsScript Instance
+    {
+        get { return instance; }
+    }
+    /* Don't destroy this object between loads */
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("The oops buttons have been pressed");
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
