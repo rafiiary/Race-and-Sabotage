d3b5014b31894295c5d19a10f4eeff38f9623962
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-08-01 19:28:15 -0400

    issue with the explanation screen reappearing fixed

--- Race and Sabotage/Assets/Scripts/next.cs
+++ Race and Sabotage/Assets/Scripts/next.cs
@@ -7,6 +7,7 @@ public class next : MonoBehaviour
     public GameObject dissappear;
     public GameObject appear;
     public GameObject button;
+    public static int checker = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +18,18 @@ public class next : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if (checker == 1)
+        //{
+        //    dissappear.gameObject.SetActive(false);
+        //    //appear.gameObject.SetActive(true);
+        //}
     }
     public void canvas()
     {
-        dissappear.SetActive(false);
+        //dissappear.SetActive(false);
+        Destroy(dissappear);
         appear.SetActive(true);
+        //checker = 1;
         button.SetActive(true);
     }
 }
